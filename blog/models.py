994f08f978b117b3d44d3668ce88f7f7a4dd12d9
Sophie Thomson <sophielbenoit@gmail.com>
2024-09-05 13:08:02 +0000

    Add a Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    # the slug is what you'll use to build a URL for each of your posts
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    content = models.TextField()
+    # auto_now_add=True means the default created time is the time of post entry
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)

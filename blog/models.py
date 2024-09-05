9234eb1178b3191b0795f4ba2a1ea99fa9c52676
Sophie Thomson <sophielbenoit@gmail.com>
2024-09-05 15:09:52 +0000

    Add Comments model

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,16 @@ class Post(models.Model):
     # auto_now_add=True means the default created time is the time of post entry
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(max_length=500, blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField(blank=False)
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
